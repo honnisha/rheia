57e195fde69614b46f07825619a91c7439f65d92
initsbriliance <ganagsh@gmail.com>
2023-03-16 18:22:55 +0300

    init

--- rust/src/main_scene.rs
+++ rust/src/main_scene.rs
@@ -1,16 +1,14 @@
-use godot::engine::node::InternalMode;
+use godot::engine::RichTextLabel;
 use godot::engine::packed_scene::GenEditState;
-use godot::engine::{Marker2D, PathFollow2D, RigidBody2D, Timer};
 use godot::prelude::*;
-use rand::Rng as _;
-use std::f64::consts::PI;
 
-// Deriving GodotClass makes the class available to Godot
 #[derive(GodotClass)]
 #[class(base=Node)]
 pub struct Main {
     #[base]
     base: Base<Node>,
+    camera: Option<Gd<Camera3D>>,
+    debug_text: Option<Gd<RichTextLabel>>,
 }
 
 #[godot_api]
@@ -26,16 +24,32 @@ impl GodotExt for Main {
     fn init(base: Base<Node>) -> Self {
         Main {
             base,
+            camera: None,
+            debug_text: None,
         }
     }
 
     fn ready(&mut self) {
+        self.camera = Some(self.base.get_node_as("Camera"));
+        self.debug_text = Some(self.base.get_node_as("Camera/DebugText"));
+
+        let camera = self.camera.as_deref_mut().unwrap();
+        camera.set_position(Vector3::new(10.0, 0.0, 10.0));
+
         godot_print!("Ready");
+    }
+
+    #[allow(unused_variables)]
+    fn process(&mut self, delta: f64) {
+        let camera = self.camera.as_deref_mut().unwrap();
+
+        let camera_pos = camera.get_position();
+        let text = format!("Camera position: {} {} {}", camera_pos.x, camera_pos.y, camera_pos.z);
+        self.debug_text.as_deref_mut().unwrap().set_text(GodotString::from(text));
     }
 }
 
-/// Root here is needs to be the same type (or a parent type) of the node that you put in the child
-///   scene as the root. For instance Spatial is used for this example.
+#[allow(dead_code)]
 fn instantiate_scene<Root>(scene: &PackedScene) -> Gd<Root>
 where
     Root: GodotClass + Inherits<Node>,
