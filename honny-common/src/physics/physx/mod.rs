2ca9d4a1ed0532b37d037f9ab696f35f64a187d0
honnisha <ganagsh@gmail.com>
2024-04-17 03:40:17 +0300

    physx

--- honny-common/src/physics/physx/mod.rs
+++ honny-common/src/physics/physx/mod.rs
@@ -11,12 +11,13 @@ use physx::{
     traits::Class,
 };
 use physx_sys::{
-    PxMeshGeometryFlags as MeshGeometryFlags, PxMeshScale_new_2, PxRigidActor, PxScene_addActor_mut,
+    PxHitFlags, PxMeshGeometryFlags as MeshGeometryFlags, PxMeshScale_new_2, PxPhysics_createShape_mut,
+    PxQueryFilterData, PxQueryFilterData_new, PxSceneQueryExt_raycastSingle, PxScene_addActor_mut,
     PxTriangleMeshGeometry_new,
 };
-use std::ffi::c_void;
 use std::ptr::null;
 use std::sync::Arc;
+use std::{ffi::c_void, mem::MaybeUninit, ptr::null_mut};
 
 // https://github.com/EmbarkStudios/physx-rs
 
@@ -43,6 +44,22 @@ type PxScene = physx::scene::PxScene<
 
 fn vec_px_from_network(from: &NetworkVector3) -> PxVec3 {
     PxVec3::new(from.x, from.y, from.z)
+}
+
+fn vec_sys_px_from_network(from: &NetworkVector3) -> physx_sys::PxVec3 {
+    physx_sys::PxVec3 {
+        x: from.x,
+        y: from.y,
+        z: from.z,
+    }
+}
+
+fn vec_network_from_px(from: &PxVec3) -> NetworkVector3 {
+    NetworkVector3::new(from.x(), from.y(), from.z())
+}
+
+fn vec_network_from_sys_px(from: &physx_sys::PxVec3) -> NetworkVector3 {
+    NetworkVector3::new(from.x, from.y, from.z)
 }
 
 /// Next up, the simulation event callbacks need to be defined, and possibly an
@@ -82,65 +99,106 @@ impl AdvanceCallback<PxArticulationLink, PxRigidDynamic> for OnAdvance {
 
 pub struct PhysxPhysicsRigidBodyEntity {
     actor: Owner<PxRigidDynamic>,
+    controller: Arc<RwLock<PhysxPhysicsController>>,
 }
 
 impl PhysxPhysicsRigidBodyEntity {
-    fn create(actor: Owner<PxRigidDynamic>) -> Self {
-        Self { actor }
+    fn create(actor: Owner<PxRigidDynamic>, controller: Arc<RwLock<PhysxPhysicsController>>) -> Self {
+        Self { actor, controller }
     }
 }
 
 impl PhysicsRigidBodyEntity for PhysxPhysicsRigidBodyEntity {
     fn set_enabled(&mut self, active: bool) {
-        todo!()
+        self.actor.enable_gravity(active)
     }
 
     fn apply_impulse(&mut self, impulse: NetworkVector3) {
-        todo!()
+        self.actor
+            .as_mut()
+            .add_force(&vec_px_from_network(&impulse), ForceMode::Impulse, true);
     }
 
     fn get_position(&self) -> NetworkVector3 {
-        todo!()
+        vec_network_from_px(&self.actor.get_global_position())
     }
 
     fn set_position(&mut self, position: NetworkVector3) {
-        todo!()
+        let pose = self.actor.get_global_pose();
+        self.actor.set_global_pose(&pose, true)
     }
 
     fn raycast(&self, dir: NetworkVector3, max_toi: f32, origin: NetworkVector3) -> Option<(usize, NetworkVector3)> {
-        todo!()
+        let controller = self.controller.as_ref().read();
+
+        let mut raycast_hit = MaybeUninit::uninit();
+
+        let filter = unsafe { PxQueryFilterData_new() };
+        if !unsafe {
+            PxSceneQueryExt_raycastSingle(
+                controller.scene.as_ptr(),
+                &vec_sys_px_from_network(&origin),
+                &vec_sys_px_from_network(&dir),
+                max_toi,
+                PxHitFlags::Default,
+                raycast_hit.as_mut_ptr(),
+                &filter as *const _,
+                null_mut(),
+                null_mut(),
+            )
+        } {
+            return None;
+        }
+
+        // SAFETY: raycastSingle returned true, so we assume buffer is initialized
+        let raycast_hit = unsafe { raycast_hit.assume_init() };
+        Some((0_usize, vec_network_from_sys_px(&raycast_hit.position)))
+
+        //Some(RaycastHit {
+        //    actor: unsafe { get_actor_entity_from_ptr(raycast_hit.actor) },
+        //    shape: unsafe { get_shape_entity_from_ptr(raycast_hit.shape) },
+        //    face_index: raycast_hit.faceIndex,
+        //    flags: raycast_hit.flags,
+        //    position: raycast_hit.position.to_bevy(),
+        //    normal: raycast_hit.normal.to_bevy(),
+        //    distance: raycast_hit.distance,
+        //})
     }
 }
 
 pub struct PhysxPhysicsCharacterController {}
 impl PhysicsCharacterController<PhysxPhysicsRigidBodyEntity> for PhysxPhysicsCharacterController {
     fn create() -> Self {
-        todo!()
+        Self {}
     }
 
     fn controller_move(&mut self, entity: &mut PhysxPhysicsRigidBodyEntity, delta: f64, impulse: NetworkVector3) {
         todo!()
+        // https://github.com/rlidwka/bevy_mod_physx/blob/ef9e56023fb7500c7e5d1f2b66057a16a3caf8d7/examples/kinematic.rs
     }
 }
 
 pub struct PhysxPhysicsStaticEntity {
     pub actor: Owner<PxRigidStatic>,
+    pub shape: Option<Owner<PxShape>>,
 }
 
 impl PhysxPhysicsStaticEntity {
     fn create(actor: Owner<PxRigidStatic>) -> Self {
-        Self { actor }
+        Self {
+            actor,
+            shape: Default::default(),
+        }
     }
 }
 
 impl PhysicsStaticEntity for PhysxPhysicsStaticEntity {
     fn remove_collider(&mut self) {
-        todo!()
+        self.actor.detach_shape(&mut self.shape.take().unwrap());
     }
 }
 
 pub struct PhysxPhysicsColliderBuilder {
-    controller: Arc<RwLock<RapierPhysicsController>>,
     collider_verts: Vec<PxVec3>,
     collider_indices: Vec<[u32; 3]>,
     geometry: Option<PxTriangleMeshGeometry>,
@@ -188,7 +246,6 @@ impl PhysxPhysicsColliderBuilder {
 impl PhysicsColliderBuilder<PhysxPhysicsStaticEntity> for PhysxPhysicsColliderBuilder {
     fn create() -> Self {
         Self {
-            controller: Arc::new(RwLock::new(RapierPhysicsController::create())),
             collider_verts: Default::default(),
             collider_indices: Default::default(),
             geometry: Default::default(),
@@ -204,8 +261,25 @@ impl PhysicsColliderBuilder<PhysxPhysicsStaticEntity> for PhysxPhysicsColliderBu
     }
 
     fn update_collider(&mut self, static_entity: &mut PhysxPhysicsStaticEntity, position: &NetworkVector3) {
-        static_entity.actor.attach_shape(shape);
-        // https://github.com/rlidwka/bevy_mod_physx/blob/ef9e56023fb7500c7e5d1f2b66057a16a3caf8d7/src/core/systems/create_actors.rs#L33
+        // let controller = self.controller.as_ref().read();
+        let mut material = controller.physics.create_material(0.5, 0.5, 0.6, ()).unwrap();
+
+        //let shape = physics.create_shape(geometry, materials, is_exclusive, shape_flags, user_data)
+        let mut shape: Owner<PxShape> = unsafe {
+            physx::shape::Shape::from_raw(
+                PxPhysics_createShape_mut(
+                    controller.physics.as_mut_ptr(),
+                    self.geometry.unwrap().as_ptr(),
+                    material.as_ptr(),
+                    true,
+                    ShapeFlags::empty(),
+                ),
+                None,
+            )
+            .unwrap()
+        };
+        static_entity.actor.attach_shape(&mut shape);
+        static_entity.shape = Some(shape);
     }
 
     fn len(&self) -> usize {
@@ -222,13 +296,13 @@ impl PhysicsColliderBuilder<PhysxPhysicsStaticEntity> for PhysxPhysicsColliderBu
 
 #[derive(Clone)]
 pub struct PhysxPhysicsContainer {
-    controller: Arc<RwLock<RapierPhysicsController>>,
+    controller: Arc<RwLock<PhysxPhysicsController>>,
 }
 
 impl PhysicsContainer<PhysxPhysicsRigidBodyEntity, PhysxPhysicsStaticEntity> for PhysxPhysicsContainer {
     fn create() -> Self {
         Self {
-            controller: Arc::new(RwLock::new(RapierPhysicsController::create())),
+            controller: Arc::new(RwLock::new(PhysxPhysicsController::create())),
         }
     }
 
@@ -269,16 +343,16 @@ impl PhysicsContainer<PhysxPhysicsRigidBodyEntity, PhysxPhysicsStaticEntity> for
         unsafe {
             PxScene_addActor_mut(controller.scene.as_mut_ptr(), actor.as_mut_ptr(), null());
         }
-        PhysxPhysicsStaticEntity::create(actor)
+        PhysxPhysicsStaticEntity::create(actor, self.controller.clone())
     }
 }
 
-pub struct RapierPhysicsController {
+pub struct PhysxPhysicsController {
     physics: PhysicsFoundation<DefaultAllocator, PxShape>,
     scene: Owner<PxScene>,
 }
 
-impl RapierPhysicsController {
+impl PhysxPhysicsController {
     fn create() -> Self {
         let mut physics = PhysicsFoundation::<_, PxShape>::default();
         let mut scene: Owner<PxScene> = physics
